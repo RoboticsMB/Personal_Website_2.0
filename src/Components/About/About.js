72eb05749ef8872d0bc1219c11d3afe1826a92f8
Matthew Bandos <mat.bandos@gmail.com>
2025-06-11 22:30:31 -0500

    Rewrote the About Me Section of my website

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,10 +3,20 @@ function About(){
     return (
         <section id="about">
             <h2>About Me</h2>
-            <p>I am passionate software engineer with a strong foundation in Computer Science and a passion for learning</p>
-            <p>I am currently pursuing a double degree in Computer Science with a concentration in Machine Learning
-            and Mathematics with a concentraion in statistics</p>
-            <p>Outside of the proffesional world, I enjoy playing tennis, chess, and skiing</p>
+                <p>I am Matthew Bandos, a college student passionate about computer science and robotics. 
+                    From a young age, I have been captivated by the ideas of automation and autonomous behavior, 
+                    and such fascination drives my learning. 
+                    I am constantly seeking opportunities to learn and expand my knowledge in the tech industry. 
+                    And one day, I hope to leverage all my experiences to design grand
+                    and novel solutions that will hopefully make the world a better place.
+                </p>
+                <p>Outside of the professional and academic worlds, I enjoy staying active. 
+                    I am a big fan of racket sports, but never afraid to try out something new! 
+                    I am also an avid chess player, skier, and big foodie! 
+                    I never pass up the opportunity to explore new places and try new cuisines! 
+                    Please feel free to contact me with any questions or further inquiries down below!
+                </p>
+
       </section>
     )
 }
