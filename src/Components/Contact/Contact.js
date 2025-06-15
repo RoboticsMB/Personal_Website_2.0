e3a02d9946b1f1a8028bf26e6cd49c2bb62a4572
Matthew Bandos <mat.bandos@gmail.com>
2025-06-14 22:00:12 -0500

    Added encompassing elements for the contact webpage

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,14 +2,16 @@ import './Contact.css'
 
 function Contact(){
     return (
-        <section id="contact">
-            <h2>Contact</h2>
-            <p>Email:</p>
-            <p>Phone: Available Upon Request</p>
-            <p>Location</p>
-            <p>LinkedIn</p>
-            <p>Github</p>
-            <p>Send me a message</p>
+        <section id="contact" className="contact-section">
+            <div className="contact-container">
+                <h2>Contact</h2>
+                <p>Email:</p>
+                <p>Phone: Available Upon Request</p>
+                <p>Location</p>
+                <p>LinkedIn</p>
+                <p>Github</p>
+                <p>Send me a message</p>\
+            </div>
       </section>
     )
 }
