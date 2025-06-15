c9909e6d93053c333a3754e977216282a3858f45
Matthew Bandos <mat.bandos@gmail.com>
2025-06-15 16:32:02 -0500

    Rewrote the text and categories for the skills section

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,19 +4,42 @@ function Skills(){
     return (
       <section id="skills">
         <h2>Skills</h2>
+        <h4>Programming Languages</h4>
         <ul>
           <li>Python</li>
+          <li>C#</li>
           <li>Java</li>
           <li>C++</li>
-          <li>C#</li>
           <li>R</li>
-          <li>Html</li>
-          <li>Css</li>
-          <li>React</li>
+        </ul>
+        <h4>Web Development</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
           <li>JavaScript</li>
+          <li>React.js</li>
+        </ul>
+        <h4>Data and Scientific Computing</h4>
+        <ul>
+          <li>Numpy</li>
+          <li>Pandas</li>
+          <li>Geopandas</li>
+          <li>Matplotlib</li>
+        </ul>
+        <h4>Automation and Testing</h4>
+        <ul>
+          <li>Playwright</li>
+          <li>Selenium</li>
+          <li>BeautifulSoup</li>
+        </ul>
+        <h4>Tooling and Workflow</h4>
+        <ul>
           <li>Git</li>
           <li>Bash</li>
-          <li>SQL</li>
+          <li>RabbitMQ</li>
+          <li>Github</li>
+          <li>Bitbucket</li>
+          <li>Jira</li>
         </ul>
       </section>
     )
