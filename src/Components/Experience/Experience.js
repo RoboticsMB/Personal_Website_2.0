ec4185363fdfd130513813227c19a5761dee8c5f
Matthew Bandos <mat.bandos@gmail.com>
2025-06-16 22:25:02 -0500

    Began adding text for the work experience section

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -4,12 +4,33 @@ function Experience(){
     return (
         <section id="experience">
             <h2>Work Experience</h2>
+            <h3>Software Engineer Intern</h3>
             <ul>
-            <li>Software Enigneer Internship (SNC)</li>
-            <li>Game Developer Apprenticeship (Simcoach Games)</li>
-            <li>Information Systems Assistant</li>
-            <li>Chess Instructor</li>
+                <li>Sierra Nevada Corporation</li>
+                <li>Location: Plano, Texas</li>
+                <li>June 2025 - August 2025</li>
+                <li>Developed customer solutions as part of the MST Division. Worked with a team of engineers to design, implement, and test software components</li>
+                <h4>Key Responsibilites</h4>
+                <li>Delivered Several Software Products to Foreign Entities</li>
+                <li>Wrote Technical Reports for Software</li>
+                <li>Assisted in High Level Design of Solutions</li>
+                <li>Performed Software Intergration</li>
             </ul>
+            <h3>IT Technician</h3>
+            <ul>
+                <li>University of Maryland Career Center</li>
+                <li>Location: College Park, Maryland</li>
+                <li>September 2024 - Present</li>
+                <li>Supported technological operations of the career center. Worked with four other IT's to troubleshoot technical issues.</li>
+                <h4>Key Responsibilites</h4>
+                <li>Resolved software, hardware, and electronic issues</li>
+                <li>Provided technical support to events on campus</li>
+                <li>Oversaw deployment and support of checkin booths for Career Fairs</li>
+                <li>Upgraded Workstations and Monitors Inside the Center</li>
+            </ul>
+            <h3>Research Assistant</h3>
+            <h3>Game Design Apprentice</h3>
+            <h3>Chess Instructor</h3>
       </section>
     )
 }
