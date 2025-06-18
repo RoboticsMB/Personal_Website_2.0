1091ccc66962f4645568cdafda2f3946ca5cec03
Matthew Bandos <mat.bandos@gmail.com>
2025-06-17 22:30:53 -0500

    Restructured content on experience.js page to be more concise and pair better with desired styling

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,70 +1,96 @@
 import './Experience.css'
 
 function Experience(){
+    const experiences = [
+        {
+            title: "Software Engineer Intern",
+            company: "Sierra Nevada Corporation",
+            location: "Plano, Texas",
+            date: "June 2025 - August 2025",
+            summary: "Developed customer solutions as part of the MST Division. Worked with a team of engineers to design, implement, and test software components",
+            responsibilites: [
+                "Delivered several software products to foreign entities",
+                "Wrote technical reports for software",
+                "Assisted in high-level design of solutions",
+                "Performed software intergration"
+            ]
+        },
+        {
+            title: "IT Technician",
+            company: "University of Maryland Career Center",
+            location: "College Park, Maryland",
+            date: "September 2024 - Present",
+            summary: "Supported technological operations of the career center. Worked with four other IT's to troubleshoot technical issues",
+            responsibilites: [
+                "Resolved software, hardware, and electronic issues",
+                "Provided technical support to events on campus",
+                "Oversaw deployment of check-in booths for career fairs",
+                "Upgraded workstations and monitors inside the center"
+            ]
+        },
+        {
+            title: "Research Assistant",
+            company: "University of Maryland Institute for Health Computing",
+            location: "College Park, Maryland",
+            date: "August 2024 - Present",
+            summary: "Conducted research as part of the GPS-Health Group. Worked with doctors, researchers, and software engineers to design new analytical methods",
+            responsibilites: [
+                "Developed automated data scrapers to extract information",
+                "Created data pipelines to process information",
+                "Assisted in writing and revising research papers",
+                "Assisted in high level design of analytical methods"
+            ]
+        },
+        {
+            title: "Game Design Apprentice",
+            company: "Simcoach Games",
+            location: "Pittsburgh, Pennsylvania",
+            date: "August 2024 - Present",
+            summary: "Developed a prototype of a transformational game. Lead three other interns in drafting, creating, and presenting the project",
+            responsibilites: [
+                "Created software implementation of game mechanics",
+                "Oversaw and delegated workload among team members",
+                "Wrote up technical report and presentation on project",
+                "Communicated actions and received feedback from supervisor"
+            ]
+        },
+                    {
+            title: "Chess Instructor",
+            company: "Gabriel Petesch Chess",
+            location: "Pittsburgh, Pennsylvania",
+            date: "August 2024 - Present",
+            summary: "Instructed students on chess theory. Worked with supervisor (International Master) to oversee administrative duties",
+            responsibilites: [
+                "Taught chess concepts and tactics to students",
+                "Adminstered and supported chess tournaments",
+                "Assisted supervisor in administrative setup",
+                "Supervised children throughout the day"
+            ]
+        }
+    ];
     return (
-        <section id="experience">
+        <section id="experienec">
             <h2>Work Experience</h2>
-            <h3>Software Engineer Intern</h3>
-            <ul>
-                <li>Sierra Nevada Corporation</li>
-                <li>Location: Plano, Texas</li>
-                <li>June 2025 - August 2025</li>
-                <li>Developed customer solutions as part of the MST Division. Worked with a team of engineers to design, implement, and test software components</li>
-                <h4>Key Responsibilites</h4>
-                <li>Delivered several software products to foreign entities</li>
-                <li>Wrote technical reports for software</li>
-                <li>Assisted in high-level design of solutions</li>
-                <li>Performed software intergration</li>
-            </ul>
-            <h3>IT Technician</h3>
-            <ul>
-                <li>University of Maryland Career Center</li>
-                <li>Location: College Park, Maryland</li>
-                <li>September 2024 - Present</li>
-                <li>Supported technological operations of the career center. Worked with four other IT's to troubleshoot technical issues</li>
-                <h4>Key Responsibilites</h4>
-                <li>Resolved software, hardware, and electronic issues</li>
-                <li>Provided technical support to events on campus</li>
-                <li>Oversaw deployment and support of check-in booths for career fairs</li>
-                <li>Upgraded workstations and monitors inside the center</li>
-            </ul>
-            <h3>Research Assistant</h3>
-            <ul>
-                <li>University of Maryland Institute for Health Computing</li>
-                <li>Location: College Park, Maryland</li>
-                <li>August 2024 - Present</li>
-                <li>Conducted research as part of the GPS-Health Group. Worked with doctors, researchers, and software engineers to design new analytical methods</li>
-                <h4>Key Responsibilites</h4>
-                <li>Developed automated data scrapers to extract information</li>
-                <li>Created data pipelines to process information</li>
-                <li>Assisted in writing and revising research papers</li>
-                <li>Assisted in high level design of analytical methods</li>
-            </ul>
-            <h3>Game Design Apprentice</h3>
-            <ul>
-                <li>Simcoach Games</li>
-                <li>Location: Pittsburgh, Pennsylvania</li>
-                <li>June 2024 - July 2024</li>
-                <li>Developed a prototype of a transformational game. Lead three other interns in drafting, creating, and presenting the project</li>
-                <h4>Key Responsibilites</h4>
-                <li>Created software implementation of game mechanics</li>
-                <li>Oversaw and delegated workload among team members</li>
-                <li>Wrote up technical report and presentation on project</li>
-                <li>Communicated actions and received feedback from supervisor</li>
-            </ul>
-            <h3>Chess Instructor</h3>
-            <ul>
-                <li>Gabriel Petesch Chess</li>
-                <li>Location: Pittsburgh, Pennsylvania</li>
-                <li>June 2023 - August 2023</li>
-                <li>Instructed students on chess theory. Worked with supervisor (International Master) to oversee administrative duties</li>
-                <h4>Key Responsibilites</h4>
-                <li>Taught chess concepts and tactics to students</li>
-                <li>Adminstered and supported chess tournaments</li>
-                <li>Assisted supervisor in administrative setup</li>
-                <li>Supervised children throughout the day</li>
-            </ul>
-      </section>
+            <div className="timeline">
+                {experiences.map((exp, index) => (
+                    <div className="timeline-item" key={index}>
+                        <div className="timeline-dot"/>
+                        <div className="timeline-content">
+                            <h3>{exp.title}</h3>
+                            <p>{exp.company}</p>
+                            <p>{exp.location} {exp.date}</p>
+                            <p>{exp.summary}</p>
+                            <p>Key Responsibilites:</p>
+                            <ul>
+                                {exp.responsibilites.map((res, idx) => (
+                                    <li key={idx}>{res}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
 
