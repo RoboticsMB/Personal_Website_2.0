37b83a6af66cac097447f3108f824f730bf95173
Matthew Bandos <mat.bandos@gmail.com>
2025-06-17 22:09:37 -0500

    Finished adding all the text to the experience.js file

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -11,26 +11,59 @@ function Experience(){
                 <li>June 2025 - August 2025</li>
                 <li>Developed customer solutions as part of the MST Division. Worked with a team of engineers to design, implement, and test software components</li>
                 <h4>Key Responsibilites</h4>
-                <li>Delivered Several Software Products to Foreign Entities</li>
-                <li>Wrote Technical Reports for Software</li>
-                <li>Assisted in High Level Design of Solutions</li>
-                <li>Performed Software Intergration</li>
+                <li>Delivered several software products to foreign entities</li>
+                <li>Wrote technical reports for software</li>
+                <li>Assisted in high-level design of solutions</li>
+                <li>Performed software intergration</li>
             </ul>
             <h3>IT Technician</h3>
             <ul>
                 <li>University of Maryland Career Center</li>
                 <li>Location: College Park, Maryland</li>
                 <li>September 2024 - Present</li>
-                <li>Supported technological operations of the career center. Worked with four other IT's to troubleshoot technical issues.</li>
+                <li>Supported technological operations of the career center. Worked with four other IT's to troubleshoot technical issues</li>
                 <h4>Key Responsibilites</h4>
                 <li>Resolved software, hardware, and electronic issues</li>
                 <li>Provided technical support to events on campus</li>
-                <li>Oversaw deployment and support of checkin booths for Career Fairs</li>
-                <li>Upgraded Workstations and Monitors Inside the Center</li>
+                <li>Oversaw deployment and support of check-in booths for career fairs</li>
+                <li>Upgraded workstations and monitors inside the center</li>
             </ul>
             <h3>Research Assistant</h3>
+            <ul>
+                <li>University of Maryland Institute for Health Computing</li>
+                <li>Location: College Park, Maryland</li>
+                <li>August 2024 - Present</li>
+                <li>Conducted research as part of the GPS-Health Group. Worked with doctors, researchers, and software engineers to design new analytical methods</li>
+                <h4>Key Responsibilites</h4>
+                <li>Developed automated data scrapers to extract information</li>
+                <li>Created data pipelines to process information</li>
+                <li>Assisted in writing and revising research papers</li>
+                <li>Assisted in high level design of analytical methods</li>
+            </ul>
             <h3>Game Design Apprentice</h3>
+            <ul>
+                <li>Simcoach Games</li>
+                <li>Location: Pittsburgh, Pennsylvania</li>
+                <li>June 2024 - July 2024</li>
+                <li>Developed a prototype of a transformational game. Lead three other interns in drafting, creating, and presenting the project</li>
+                <h4>Key Responsibilites</h4>
+                <li>Created software implementation of game mechanics</li>
+                <li>Oversaw and delegated workload among team members</li>
+                <li>Wrote up technical report and presentation on project</li>
+                <li>Communicated actions and received feedback from supervisor</li>
+            </ul>
             <h3>Chess Instructor</h3>
+            <ul>
+                <li>Gabriel Petesch Chess</li>
+                <li>Location: Pittsburgh, Pennsylvania</li>
+                <li>June 2023 - August 2023</li>
+                <li>Instructed students on chess theory. Worked with supervisor (International Master) to oversee administrative duties</li>
+                <h4>Key Responsibilites</h4>
+                <li>Taught chess concepts and tactics to students</li>
+                <li>Adminstered and supported chess tournaments</li>
+                <li>Assisted supervisor in administrative setup</li>
+                <li>Supervised children throughout the day</li>
+            </ul>
       </section>
     )
 }
