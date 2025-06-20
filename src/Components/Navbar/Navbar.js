a58ac554fed4c714bee0c557e8f90f5d6beca3c0
Matthew Bandos <mat.bandos@gmail.com>
2025-06-20 14:09:01 -0500

    Continued styling the project boxes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -43,7 +43,7 @@ function Navbar(){
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#experience">Experience</a></li>
-                <li><a href="#research/projects">Projects</a></li>
+                <li><a href="#technicalprojects">Projects</a></li>
                 <li><a href="#education">Education</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
@@ -55,7 +55,7 @@ function Navbar(){
                         <li><a href="#about" onClick={closeMenu}>About</a></li>
                         <li><a href="#skills" onClick={closeMenu}>Skills</a></li>
                         <li><a href="#experience" onClick={closeMenu}>Experience</a></li>
-                        <li><a href="#research/projects" onClick={closeMenu}>Projects</a></li>
+                        <li><a href="#technicalprojects" onClick={closeMenu}>Projects</a></li>
                         <li><a href="#education" onClick={closeMenu}>Education</a></li>
                         <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
                     </ul>
--- src/Components/TechnicalProjects/TechnicalProjects.js
+++ src/Components/TechnicalProjects/TechnicalProjects.js
@@ -3,8 +3,8 @@ import './TechnicalProjects.css'
 function TechnicalProjects(){
     const projects = [
         {
-            title: "This Website",
-            details: ["A responsive personal website showcasing my skills, projects, and experiences. Built from the ground up, completely from scratch"],
+            title: "Personal Websites",
+            details: ["2 Fully Published Websites", "Built From Scratch", "Responsive Mobile & Desktop Design"],
             language: "React",
             codeBase: "https://github.com/RoboticsMB/Personal_Website_2.0"
         },
@@ -15,8 +15,8 @@ function TechnicalProjects(){
             codeBase: "RESTRICTED"
         },
         {
-            title: "Robotics Club CodeBase",
-            details: ["Software Integration for 90lb robot", "5k+ lines for code architecture/yr", "Code Base: 2022, 2023, 2024"
+            title: "Robotics Club Codebase",
+            details: ["Software Integration for 90lb robot", "5k+ Lines for Code Architecture/yr", "Code Base: 2022, 2023, 2024"
             ],
             language: "C++ & Java",
             codeBase: "https://github.com/quasics/quasics-frc-sw-2015/tree/master"
@@ -35,9 +35,17 @@ function TechnicalProjects(){
             <div className="projects">
                 {projects.map((prj, index) => (
                     <div className="project-item" key={index}>
-                        <div className="project-content">
+                        <div className="project-title-container">
                             <h3 className="project-title">{prj.title}</h3>
-                            <p className="project-details">{prj.details}</p>
+                        </div>
+                        <div className="project-details-container">
+                            <div className="project-tags">
+                                {prj.details.map((detail, i) => (
+                                    <span className="project-tag" key={i}>
+                                        {detail}
+                                    </span>
+                                ))} 
+                            </div>
                             <p className="project-language">{prj.language}</p>
                             <p className="project-codebase">{prj.codeBase}</p>
                         </div>
