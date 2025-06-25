2eb8062a4494300b18f82576472a8b844008b4a1
Matthew Bandos <mat.bandos@gmail.com>
2025-06-24 21:32:52 -0500

    Added Functionality to succesfully messages on my form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,8 +2,40 @@ import './Contact.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone, faLocationDot, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import React from 'react'
+import Swal from 'sweetalert2'
 
 function Contact(){
+    // const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        // setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "f95647ed-44e8-4968-bc9b-fd15f721ad92");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            Swal.fire({
+            title: "Success!",
+            text: "Message sent successfully!",
+            icon: "success"
+            });
+        // setResult("Form Submitted Successfully");
+        // event.target.reset();
+        } else {
+        console.log("Error", data);
+        // setResult(data.message);
+        }
+    };
+
     const contacts = [
         {
             title: "Email",
@@ -32,7 +64,6 @@ function Contact(){
         }
     ]
 
-
     return (
         <section id="contact" className="contact-section">
             <h2>Get in <span className="color-touch">Touch</span></h2>
@@ -49,15 +80,16 @@ function Contact(){
                 </div>
                 <div className="contact-form">
                     <h3>Send Me a Message</h3>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <p>Name</p>
-                        <input type="text" required/>
+                        <input type="text" name="name" required/>
                         <p>Email</p>
-                        <input type="email" required/>
+                        <input type="email" name="email" required/>
                         <p>Message</p>
-                        <textarea required></textarea>
+                        <textarea name="message" required></textarea>
                         <button type="submit"><FontAwesomeIcon icon={faPaperPlane} />Send Message</button>
                     </form>
+                    {/* <span>{result}</span> */}
                 </div>
             </div>
       </section>
